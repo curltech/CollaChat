ff5fa0ccee3e79d3de01e4e5841e178f54d61647
visualfengs <visualfengs@gmail.com>
2022-01-02 15:43:58 +0800

    0.2.73

--- src/pages/login.vue.js
+++ src/pages/login.vue.js
@@ -632,7 +632,7 @@ export default {
     upgradeVersion(flag) {
       let _that = this
       let store = _that.$store
-      store.currentVersion = '0.2.72'
+      store.currentVersion = '0.2.73'
       store.mandatory = false
       if (_that.versionHistory && _that.versionHistory.length > 0) {
         let no = 1
