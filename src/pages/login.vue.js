5130b9656b73125f57e3b13ae3f737b6a3485e36
visualfengs <visualfengs@gmail.com>
2022-01-18 00:54:54 +0800

    1.0.3

--- src/pages/login.vue.js
+++ src/pages/login.vue.js
@@ -625,7 +625,7 @@ export default {
     upgradeVersion(flag) {
       let _that = this
       let store = _that.$store
-      store.currentVersion = "1.0.2"
+      store.currentVersion = "1.0.3"
       store.mandatory = false
       if (_that.versionHistory && _that.versionHistory.length > 0) {
         let no = 1
